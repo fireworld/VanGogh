1b03e94a1418565b6020a991d6b8a2c1c98d2127
cxx <xx.ch@outlook.com>
2017-09-04 00:01:43 +0800

    developing

--- vangogh/src/main/java/cc/colorcat/vangogh/NetworkInterceptor.java
+++ vangogh/src/main/java/cc/colorcat/vangogh/NetworkInterceptor.java
@@ -17,7 +17,8 @@ class NetworkInterceptor implements Interceptor {
     public Result intercept(Chain chain) throws IOException {
         Task task = chain.task();
         int fromPolicy = task.fromPolicy() & From.NETWORK.policy;
-        if (fromPolicy != 0 && task.uri().getScheme().startsWith("http")) {
+        String scheme = task.uri().getScheme();
+        if (fromPolicy != 0 && ("http".equals(scheme) || "https".equals(scheme))) {
             Downloader downloader = chain.loader();
             Result result = downloader.load(vanGogh, task);
             if (result != null) {
