569ccc5af5e5349beb1643f93ffa792d7f682707
cxx <xx.ch@outlook.com>
2017-12-04 22:20:31 +0800

    none

--- app/src/main/java/cc/colorcat/vangoghdemo/view/CourseActivity.java
+++ app/src/main/java/cc/colorcat/vangoghdemo/view/CourseActivity.java
@@ -165,7 +165,6 @@ public class CourseActivity extends BaseActivity implements ICourses.View {
                 mAdapter.notifyItemRangeRemoved(1, 3);
                 mCourses.addAll(2, moved);
                 mAdapter.notifyItemRangeInserted(2, 3);
-//                mAdapter.notifyDataSetChanged();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
