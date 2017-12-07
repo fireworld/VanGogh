3ad982afd17b48b1453c963b31d72d183c48b3de
cxx <xx.ch@outlook.com>
2017-12-07 15:16:24 +0800

    1. add AbsTarget

---
+++ library/src/main/java/cc/colorcat/vangogh/AbsTarget.java
@@ -0,0 +1,58 @@
+package cc.colorcat.vangogh;
+
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by cxx on 17-12-7.
+ * xx.ch@outlook.com
+ */
+public abstract class AbsTarget<V extends View> implements Target {
+    private static final int TAG_ID = R.string.app_name;
+
+    private final Reference<? extends V> ref;
+    private final Object tag;
+
+    public AbsTarget(V view, Object tag) {
+        this.ref = new WeakReference<>(view);
+        this.tag = tag;
+    }
+
+    @Override
+    public void onPrepare(@Nullable Drawable placeHolder) {
+        V view = ref.get();
+        if (view != null) {
+            setDrawable(view, placeHolder);
+            view.setTag(TAG_ID, tag);
+        }
+    }
+
+    @Override
+    public void onLoaded(Drawable drawable, From from) {
+        setDrawableWithCheck(drawable);
+    }
+
+    @Override
+    public void onFailed(@Nullable Drawable error, Exception cause) {
+        setDrawableWithCheck(error);
+        LogUtils.e(cause);
+    }
+
+    private void setDrawableWithCheck(Drawable drawable) {
+        V view = ref.get();
+        if (view != null && checkTag(view)) {
+            setDrawable(view, drawable);
+        }
+    }
+
+    private boolean checkTag(V view) {
+        Object obj = view.getTag(TAG_ID);
+        return tag == obj || (tag != null && tag.equals(obj));
+    }
+
+    protected abstract void setDrawable(V view, Drawable drawable);
+}
